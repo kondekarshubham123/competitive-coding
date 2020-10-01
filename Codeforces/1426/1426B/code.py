3a41ce3b08edd43cf1c0357179252e3ffd70d399
Shubham Kondekar <kondekarshubham123@gmail.com>
2020-10-01 23:55:36 +0000

    [Solved][Python3] 1426B

--- Codeforces/1426/1426B/code.py
+++ Codeforces/1426/1426B/code.py
@@ -1,6 +1,22 @@
+FunLL = lambda lis:lis[0][0]==lis[1][1]
+FunRR = lambda lis:lis[0][1]==lis[1][0]
+
+
+
 for tt in range(int(input())):
     n,m = map(int,input().split())
-    arr = [[list(map(int,input().split()))]+[list(map(int,input().split()))] for _ in range(n)]
-
-
-    print(arr)
+    LL,RR,LR = False,False,False
+    arrays = [[list(map(int,input().split()))]+[list(map(int,input().split()))] for _ in range(n)]
+    if m%2!=0:
+        print("NO")
+        continue
+    
+    for array in arrays:
+        if FunLL(array) and FunRR(array):
+            LR = 1
+            break
+        elif FunRR(array):
+            RR = True
+            break
+    print("YES" if (LR==1 or RR == 1) else "NO")
+    
