a730dddafdf81561e4a61fed51fd45477291cf9f
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-02-18 21:16:02 +0530

    convertbits a to b using native

--- The DSA-One Course by Anuj sir/Bit Manipulation/bits_req_to_convrt.py
+++ The DSA-One Course by Anuj sir/Bit Manipulation/bits_req_to_convrt.py
@@ -11,10 +11,13 @@
 ## find number of set bit 
 
 # Navtive method 
-
+def countSet2(numm):
+    # print(bin(numm))
+    return bin(numm).count('1')
 
 # Trick n & n-1
 def countSet(val):
+    
     if val&val-1 == 0:
         return 0
     return 1 + countSet(val&val-1)
@@ -25,5 +28,7 @@ def aconvertb(a,b):
     # print(bin(a)[2:]+"\n"+bin(b)[2:])
     ans = a^b
     # print(ans,bin(ans)[2:])
-    return countSet(ans)
-print(aconvertb(26,21) + 1)
+    return countSet2(ans)
+
+print(aconvertb(25,12)) # for countSet
+print(aconvertb(25,12)) # for countSet2
