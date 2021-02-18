9692af06f06c839ab3e9c19ce690b0979fec9a52
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-02-18 12:16:29 +0530

    mul and div by 2 up to n

--- The DSA-One Course by Anuj sir/Bit Manipulation/mulndiv2.py
+++ The DSA-One Course by Anuj sir/Bit Manipulation/mulndiv2.py
@@ -6,6 +6,45 @@ def divbytwo(num):
 def mulbytwo(num):
     return num << 1
 
-print(divbytwo(14)) # output -> 7
-print(divbytwo(15)) # output -> 7
-print(mulbytwo(14)) # output -> 28
+# print(divbytwo(14)) # output -> 7
+# print(divbytwo(15)) # output -> 7
+# print(mulbytwo(14)) # output -> 28
+
+# ---------------- xxx ---------------------------- #
+
+# function two multiply a number with 2 by n times
+
+# Example 
+# input : num = 14, n = 4
+# output: 224 
+# Explaination
+# n     express     num 
+# 1     14 * 2      28
+# 2     28 * 2      56
+# 3     26 * 2      112
+# 4     112* 2      224
+
+
+def mulntimetwo(num,n):
+    return num << n 
+
+# print(mulntimetwo(14,3))
+
+# ---------------- xxx ---------------------------- #
+
+# function two division a number with 2 by n times
+
+# Example 
+# input : num = 14, n = 2
+# output: 224 
+# Explaination
+# n     express     num 
+# 1     14 // 2      7
+# 2     7 // 2       3
+# 3     3 // 2       1
+
+
+def divntimetwo(num,n):
+    return num >> n 
+
+print(divntimetwo(14,3))
