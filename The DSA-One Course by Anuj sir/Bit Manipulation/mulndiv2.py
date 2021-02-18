985a0b12054bf8ceff947755ecabcc2e3476f298
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-02-18 12:06:29 +0530

    mulndiv2 added

---
+++ The DSA-One Course by Anuj sir/Bit Manipulation/mulndiv2.py
@@ -0,0 +1,11 @@
+# fun to make integer division
+def divbytwo(num):
+    return num >> 1
+
+# function which return num * 2
+def mulbytwo(num):
+    return num << 1
+
+print(divbytwo(14)) # output -> 7
+print(divbytwo(15)) # output -> 7
+print(mulbytwo(14)) # output -> 28
