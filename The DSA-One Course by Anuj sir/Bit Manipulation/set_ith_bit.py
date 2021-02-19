0e823670883d201a1c6e25489eb89790d61c2f44
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-02-19 12:51:05 +0530

    Unset bit program solved

--- The DSA-One Course by Anuj sir/Bit Manipulation/set_ith_bit.py
+++ The DSA-One Course by Anuj sir/Bit Manipulation/set_ith_bit.py
@@ -13,3 +13,22 @@ def set_ith_bit(num,i):
 num = 26
 i = 3
 print(set_ith_bit(num,i))
+
+# --------------------------------
+# Program to unset ith bit
+
+# example :
+# bin = 11010
+# i = 1
+# outbin = 11000
+
+def unset_ith_bit(num,i):
+    # print(bin(num)[2:],i)
+    shi = 1 << i
+    # print(bin(shi)[2:].rjust(len(bin(num))-2,'0'))
+    # print(bin(~shi))
+    
+    # print(bin(~((~(shi))^num))[2:])
+    return ~((~shi)^num)
+
+print(unset_ith_bit(num,i))
